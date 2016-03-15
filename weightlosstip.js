d20802b8b2c8f79e1fbe369b11a94b3a29764078
lva244 <lvanh24494@gmail.com>
2016-03-15 13:25:11 +0700

    fix

--- weightlosstip.js
+++ weightlosstip.js
@@ -13,7 +13,7 @@ function checkCookie() {
         suggest_video();
     } else {
         var thumbnail = get_embed(get_parameter('vid='), 'thumbnail_url');
-        $('#new').append('<div id="btn" style="display: inline-block;"><img src="'+thumbnail+'"/ width="40" height="30" ><div style="display: inline-block; color: red;"><p><small>WATCH VIDEO HERE.<br><i>This function to protect you from DOS attacks, hope you enjoy watching video</i></small></p></div></div>');
+        $('#new').append('<div id="btn" style="display: inline-block;  color: red;"><img src="'+thumbnail+'"/ width="65" height="40" ><div style="display: inline-block;"><p>WATCH VIDEO HERE.</p></div><i>This function to protect you from DOS attacks, hope you enjoy watching video</i></div>');
     }
 }
 
